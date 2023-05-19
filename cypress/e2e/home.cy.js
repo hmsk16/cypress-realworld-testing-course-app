e3f90302b5c14c8c070215e6162bb615c055e9ec
hmsk16 <sohaibkhalid16@gmail.com>
2023-05-19 19:20:46 +0500

    New test caese

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,12 @@
+import { contains } from "lodash/fp"
+
+describe('Home page', () => {
+  it('Visit the site', () => {
+    cy.visit("http://localhost:3000/")
+    cy.get("[data-test='hero-heading']").contains("Testing Next.js Applications with Cypress")
+  })
+
+  it.only("the features on the homepage are correct", () => {
+    cy.visit("http://localhost:3000")
+  })
+})
