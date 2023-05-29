03e2a33c58cefe722a8b8315a998fce8d2f9b6be
hmsk16 <sohaibkhalid16@gmail.com>
2023-05-29 19:54:02 +0500

    Test cases in sections

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -15,25 +15,26 @@ describe('Home page', () => {
       cy.get("dt").eq(2).contains("Free and Open Source")
     })
   })
+  
+  context("Email Section", () => {
+    it("Subscribe for Updates", () => {
+      cy.get("#email").type("sohaibkhalid12@gmail.com")
+      cy.get("[data-test='submit-button']").click()
+      cy.get("[data-test='success-message']").should("exist").contains("Success: sohaibkhalid12@gmail.com has been successfully subscribed")
+    })
 
-  // it("Subscribe for Updates", () => {
-  //   cy.get("#email").type("sohaibkhalid12@gmail.com")
-  //   cy.get("[data-test='submit-button']").click()
-  //   cy.get("[data-test='success-message']").should("exist").contains("Success: sohaibkhalid12@gmail.com has been successfully subscribed")
-  // })
+    it("Not Subscribe for Updates", () => {
+      cy.get("#email").type("sohaibkhalid12")
+      cy.get("[data-test='submit-button']").click()
+      cy.get("[data-test='success-message']").should("not.exist")
+    })
 
-  // it("Not Subscribe for Updates", () => {
-  //   cy.get("#email").type("sohaibkhalid12")
-  //   cy.get("[data-test='submit-button']").click()
-  //   cy.get("[data-test='success-message']").should("not.exist")
-  // })
-
-  // it("Subscribe for Updates with another email", () => {
-  //   cy.get("#email").type("john@example.com")
-  //   cy.get("[data-test='submit-button']").click()
-  //   cy.get("[data-test='server-error-message']").should("exist").contains("Error: john@example.com already exists. Please use a different email address.")
-  // })
-
+    it("Subscribe for Updates with another email", () => {
+      cy.get("#email").type("john@example.com")
+      cy.get("[data-test='submit-button']").click()
+      cy.get("[data-test='server-error-message']").should("exist").contains("Error: john@example.com already exists. Please use a different email address.")
+    })
+  })
   context("Courses section", () => {
     it("Testing Your First Next.js Application", () => {
       cy.get("[data-test='course-0']").find('a').contains('Get started').click();
